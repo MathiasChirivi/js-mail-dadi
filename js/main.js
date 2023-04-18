62bd203a9e4f3dc5be37a4eb381acbe645c1abde
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-18 15:29:11 +0200

    versione base

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//Chiedi all’utente la sua email.
+//Controlla che sia nella lista di chi può accedere,
+//Stampa un messaggio appropriato sull’esito del controllo. 
+//Versione di base usando prompt e console.log/alert.
+
+
+//Creazione Email (Array)
+const email = ['chirivimathias@gmail.com', 'mathiaschirivi@gmail.com', 'luca@icloud.com', 'antoniogirolamo@email.com' ];
+
+var tuaEmail = prompt("Inserisci qua la tua email per controllare se sei in lista.").value;
+console.log(tuaEmail);
+
+var identificazioneEmail = false;
+
+for (let i = 0; i < email.length; i++) {
+    var Mail = email[i];
+    console.log(Mail)
+    
+    if (Mail == tuaEmail) {
+        identificazioneEmail = true;
+    }
+}
+    var identificazione = email;
+
+    if(identificazioneEmail == true){
+        alert('Benvenuto, sei libero di accedere')
+    } else {
+        alert('non sei registrato è pregato di aggiungere un email esistente')
+    }
